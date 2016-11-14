f71faf58a037b738332fe0ee81c3e8ad2295e499
kory donati <koryd@bixly.com>
2016-11-13 23:01:32 -0800

    finalized

--- imagezip/pipeline.go
+++ imagezip/pipeline.go
@@ -1,33 +1,40 @@
 package imagezip
 
 import (
-	_ "compress/gzip"
-	"errors"
-	"fmt"
-	//"io/ioutil"
-	_ "archive/tar"
+	"archive/zip"
 	"bufio"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"archive/zip"
 	"sync"
+	"time"
 )
 
-func ReadImageFile(filepath string, halt chan int, wg *sync.WaitGroup) (chan string, error) {
+const (
+	BYTE_IMAGE_CHAN_SIZE = 100
+	URL_CHAN_SIZE = 200
+)
 
-	out_chan := make(chan string, 20)
+func ReadImageFile(filepath string, wg *sync.WaitGroup) (chan string, error) {
 
+	out_chan := make(chan string, URL_CHAN_SIZE)
+
+	go readImage(filepath, out_chan, wg)
+
+	return out_chan, nil
+
+}
+
+func readImage(filepath string, out_chan chan string, wg *sync.WaitGroup) {
 	fid, err := os.Open(filepath)
+	defer fid.Close()
 	if err != nil {
 		fmt.Println("error", err)
-		return out_chan, err
+		return
 	}
 	lines := bufio.NewReader(fid)
-
-	//lines := strings.Split(string(fid), "\n")
-
 	for {
 		line, _, err := lines.ReadLine()
 		strLine := string(line)
@@ -35,60 +42,51 @@ func ReadImageFile(filepath string, halt chan int, wg *sync.WaitGroup) (chan str
 			fmt.Println("end of file...")
 			break
 		}
-		select {
-		case <-halt:
-			fmt.Println("Halt detected")
-			return out_chan, errors.New("Halted...")
-		default:
-			go func() {
-				if strLine == "" {
-					return
-				}
-				wg.Add(1)
-				out_chan <- strLine
-				return
-			}()
+		if strLine == "" {
+			return
 		}
+		wg.Add(1)
+		out_chan <- strLine
 	}
-
-	return out_chan, err
-
 }
 
 func GetImages(out chan string) chan ByteImage {
-	tex := make(chan ByteImage, 10)
-	go func() {
-		for url := range out {
-			go func(url string) {
-				resp, err := http.Get(url)
-				if err != nil {
-					fmt.Println("error in get images", err)
-					return
-				}
-				fid, err := ioutil.TempFile("out/", "image_")
-				defer fid.Close()
-				if err != nil {
-					fmt.Println("err creating temp file", err)
-				}
-
-				fmt.Println("stats", resp.Status)
-				//defer resp.Body.Close()
-				_, err = io.Copy(fid, resp.Body)
-				if err != nil {
-					fmt.Println("error copy body to temp file", err)
-					os.Remove(fid.Name())
-				}
-				blobImage := NewByteImage(fid.Name())
-
-				if err != nil {
-					fmt.Println("read error", err)
-				}
-				tex <- blobImage
-				return
-			}(url)
-		}
-	}()
+	tex := make(chan ByteImage, BYTE_IMAGE_CHAN_SIZE)
+	go getImagesLoop(out, tex)
 	return tex
+}
+
+func getImagesLoop(out chan string, tex chan ByteImage) {
+	for url := range out {
+		go getImage(url, tex)
+	}
+}
+
+func getImage(url string, tex chan ByteImage) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error in get images", err)
+		return
+	}
+	fid, err := ioutil.TempFile("out/", "image_")
+	defer fid.Close()
+	if err != nil {
+		fmt.Println("err creating temp file", err)
+	}
+
+	fmt.Println("stats", resp.Status)
+	_, err = io.Copy(fid, resp.Body)
+	if err != nil {
+		fmt.Println("error copy body to temp file", err)
+		os.Remove(fid.Name())
+	}
+	blobImage := NewByteImage(fid.Name())
+
+	if err != nil {
+		fmt.Println("read error", err)
+	}
+	tex <- blobImage
+	return
 }
 
 func WriteZip(tex chan ByteImage, wg *sync.WaitGroup) *zip.Writer {
@@ -99,59 +97,55 @@ func WriteZip(tex chan ByteImage, wg *sync.WaitGroup) *zip.Writer {
 	if err != nil {
 		fmt.Println("good stuff... not", err)
 	}
-	//tw := tar.NewWriter(my_file)
 	tw := zip.NewWriter(my_file)
 
-	//defer my_file.Close()
-
-	go func() {
-		for blob := range tex {
-
-			//defer blob.resp.Close()
-
-			myfile, err := os.Open(blob.file_name)
-			if err != nil {
-				fmt.Println("errer open tempfile", err)
-			}
-
-			wr, err := tw.Create(blob.file_name)
-			if err != nil {
-				fmt.Println("error creating thing", err)
-			}
-
-			io.Copy(wr, myfile)
-			wg.Done()
-			myfile.Close()
-			os.Remove(myfile.Name())
-
-
-
-			//fstat, err := myfile.Stat()
-			//if err != nil {
-			//	fmt.Println("there was an error", err)
-			//}
-			//header, err := tar.FileInfoHeader(fstat, blob.file_name)
-			//if err != nil {
-			//	fmt.Println("err or in fileinfoheardr", err)
-			//}
-			//
-			//fmt.Println("File header info:", header)
-			//
-			//tw.WriteHeader(header)
-			//temp, err := ioutil.ReadAll(blob.resp)
-			//if err != nil {
-			//	fmt.Println("error reading file", err)
-			//}
-			//
-			//_, err = tw.Write(temp)
-			//if err != nil {
-			//	fmt.Println("error write", err)
-			//}
-
-			//io.Copy(tw, myfile)
-			//tw.Flush()
-
-		}
-	}()
+	go writeBlobtoZip(tw, tex, wg)
 	return tw
+}
+
+func writeBlobtoZip(tw *zip.Writer, tex chan ByteImage, wg *sync.WaitGroup) {
+	for blob := range tex {
+
+		//defer blob.resp.Close()
+
+		myfile, err := os.Open(blob.file_name)
+		if err != nil {
+			fmt.Println("errer open tempfile", err)
+		}
+
+		wr, err := tw.Create(blob.file_name)
+		if err != nil {
+			fmt.Println("error creating thing", err)
+		}
+
+		io.Copy(wr, myfile)
+		wg.Done()
+		myfile.Close()
+		os.Remove(myfile.Name())
+
+	}
+}
+
+
+func StartPipeline(file string){
+	start := time.Now()
+	var wg sync.WaitGroup
+	//var utl string
+	wg.Add(1)
+	out, err := ReadImageFile(file, &wg)
+
+	if err != nil {
+		fmt.Println("There was an error", err)
+		return
+	}
+
+	tex := GetImages(out)
+	fmt.Println("main done with getimages")
+
+	tw := WriteZip(tex, &wg)
+	defer tw.Close()
+	wg.Wait()
+	end := time.Since(start)
+	total := time.Duration(end)
+	fmt.Println("total time:", total, end)
 }
--- main.go
+++ main.go
@@ -1,38 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kd2718/goimgzip/imagezip"
-	"sync"
-	"time"
 )
 
 func main() {
-	start := time.Now()
-	fmt.Println("Heyyyy")
 
 	file := "input/img_urls.txt"
-	var wg sync.WaitGroup
-	halt := make(chan int, 20)
-	defer close(halt)
-	//var utl string
-	wg.Add(1)
-	out, err := imagezip.ReadImageFile(file, halt, &wg)
 
-	if err != nil {
-		fmt.Println("There was an error", err)
-		return
-	}
+	imagezip.StartPipeline(file)
 
-	tex := imagezip.GetImages(out)
-	_ = tex
-	fmt.Println("main done with getimages")
-
-	tw := imagezip.WriteZip(tex, &wg)
-	defer tw.Close()
-	wg.Wait()
-	end := time.Since(start)
-	total := time.Duration(end)
-	fmt.Println("total time:", total, end)
 
 }
